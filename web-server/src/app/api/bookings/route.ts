55f9a43fcbbf9c9ad7e354766e172aa0721721fb
josephan-justin <josephanjustin2@gmail.com>
2026-09-12 09:15:29 +0700

    feat(server) : add POST booking (user)

---
+++ web-server/src/app/api/bookings/route.ts
@@ -0,0 +1,58 @@
+import { BadRequestError } from "@/server/helpers/customError";
+import { errorHandler } from "@/server/helpers/errorHandler";
+import Booking, { bookingSchema } from "@/server/models/Booking";
+import Vehicle from "@/server/models/Vehicle";
+import Workshop from "@/server/models/Workshops";
+
+
+export async function POST(request: Request) {
+    try {
+        const userId = request.headers.get("x-user-id")
+        if(!userId){
+            throw new BadRequestError("User id is required")
+        }
+
+        const body = await request.json()
+        const vehicle = await Vehicle.where("_id", body.vehicle_id).where("user_id", userId).first()
+
+        if(!vehicle){
+            throw new BadRequestError("Vehicle not found")
+        }
+
+        const workshop = await Workshop.where("_id", body.bengkel_id).where("is_active", true).first()
+
+        if(!workshop){
+            throw new BadRequestError("Workshop not found")
+        }
+
+        const bookingCode = `CM-${Date.now()}`
+        const validated = bookingSchema.parse({
+            ...body,
+            booking_code: bookingCode,
+            user_id: userId,
+            status: "pending"
+        })      
+        
+        const booking = await Booking.insert({
+            booking_code: validated.booking_code,
+            user_id: validated.user_id,
+            bengkel_id: validated.bengkel_id,
+            vehicle_id: validated.vehicle_id,
+            booking_date: validated.booking_date,
+            booking_time_slot: validated.booking_time_slot,
+            status: validated.status,
+            services_done: validated.services_done,
+            total_price: validated.total_price,
+            pending_tasks: validated.pending_tasks,
+            notes: validated.notes,
+            report_pdf_url: validated.report_pdf_url
+        })
+
+        return Response.json(booking, {status: 201})
+    } catch (error: unknown) {
+        const { message, status } = errorHandler(error);
+    
+        return Response.json({ message }, { status });
+        
+    }
+}
--- web-server/src/proxy.ts
+++ web-server/src/proxy.ts
@@ -1,3 +1,7 @@
+import dns from "node:dns";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 import { NextRequest, NextResponse } from "next/server";
 import { UnauthorizedError } from "./server/helpers/customError";
 import { verifyToken } from "./server/helpers/jwt";
