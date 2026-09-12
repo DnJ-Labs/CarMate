ff767006182228125714af4457037f5b1b083b97
josephan-justin <josephanjustin2@gmail.com>
2026-09-12 10:51:54 +0700

    fix(server) : authorization  POST & GET vehicle

--- web-server/src/app/api/vehicles/route.ts
+++ web-server/src/app/api/vehicles/route.ts
@@ -1,9 +1,15 @@
+import { BadRequestError } from "@/server/helpers/customError";
 import { errorHandler } from "@/server/helpers/errorHandler";
 import Vehicle, { IVehicle, vehicleSchema } from "@/server/models/Vehicle";
 
-export async function GET() {
+export async function GET(request: Request) {
   try {
-    const vehicles: IVehicle[] = await Vehicle.get();
+    const userId = request.headers.get("x-user-id")
+    if(!userId){
+            throw new BadRequestError("User Id is required")
+        }
+
+    const vehicles: IVehicle[] = await Vehicle.where("user_id", userId).get();
     return Response.json(vehicles, {
       status: 200,
     });
@@ -15,10 +21,14 @@ export async function GET() {
 
 export async function POST(request: Request) {
   try {
+    const userId = request.headers.get("x-user-id");
+    if (!userId) {
+      throw new BadRequestError("User id is required");
+    }
     const body = await request.json();
-    const validated = vehicleSchema.parse(body);
+    const validated = vehicleSchema.parse({...body, user_id: userId});
     const vehicle = await Vehicle.insert({
-      user_id: validated.user_id,
+      user_id: userId,
       brand: validated.brand,
       model: validated.model,
       plate_number: validated.plate_number,
